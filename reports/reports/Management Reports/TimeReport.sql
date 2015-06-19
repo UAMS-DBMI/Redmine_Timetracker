77e369d997baeb05a3af60518e5611a15750e8a6
RutherfordMichaelW <mwrutherford@uams.edu>
2015-06-19 11:37:59 -0500

    reorganize

--- reports/reports/Management Reports/TimeReport.sql
+++ reports/reports/Management Reports/TimeReport.sql
@@ -5,7 +5,7 @@
 --      name: "date_range",
 --      display: "Date Range",
 --      type: "daterange", 
---      default: { start: "yesterday", end: "yesterday" }
+--      default: { start: "today", end: "today" }
 -- }
 -- FILTER: { 
 --      column: "Customer Name", 
@@ -17,19 +17,22 @@
 -- }
 
 SELECT p.project_id, p.name, p.lft, p.depth,
-       t.time_entry_id, t.user_id, t.issue_id,
+       t.time_entry_id, t.user_id, t.user_name,
+       t.issue_id, t.issue_subject,
        t.hours, t.spent_on, t.activity_id,
        t.effort_values, t.comments
   FROM project_info p
   LEFT JOIN time_entry_info t
     ON t.project_id = p.project_id
+ WHERE 1 = 1
+   AND (t.spent_on BETWEEN "{{ date_range.start }}" AND "{{ date_range.end }}")
 /*
  WHERE 1 = 1
-   AND ($project_id IS NULL OR p.project_id = $project_id)
-   AND ($user_id IS NULL OR t.user_id = $user_id)
-   AND ($spent_on_start IS NULL OR t.spent_on BETWEEN $spent_on_start AND $spent_on_end)
-   AND (NULL IS NULL OR t.activity_id = NULL)
-   AND ($effort_values IS NULL OR t.effort_values REGEXP $effort_values);  --must have single quotes and | between values for regexp
+   AND (t.spent_on BETWEEN "{{ date_range.start }}" AND "{{ date_range.end }}")
+   AND ("{{ project }}" IS NULL OR p.project_id = "{{ project }}")
+   AND ("{{ user }}" IS NULL OR t.user_id = "{{ user }}")
+   AND ("{{ activity }}" IS NULL OR t.activity_id = "{{ activity }}")
+   AND ("{{ efforts }}" IS NULL OR t.effort_values REGEXP "{{ efforts }}");  #must have single quotes and | between values for regexp
 
 
 
