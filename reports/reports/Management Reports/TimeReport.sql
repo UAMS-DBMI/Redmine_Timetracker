a4d4584b74759d1935151e4549c073d2951c63c1
RutherfordMichaelW <mwrutherford@uams.edu>
2015-06-30 09:49:22 -0500

    Report work

--- reports/reports/Management Reports/TimeReport.sql
+++ reports/reports/Management Reports/TimeReport.sql
@@ -8,29 +8,81 @@
 --      default: { start: "today", end: "today" }
 -- }
 -- VARIABLE: {
+--      name: "project_filter",
+--      display: "Project",
+--      type: "select",
+--      database_options: {
+--          table: "project_drop",
+--          column: "name",
+--          all: true
+--      },
+--      multiple: false
+-- }
+-- VARIABLE: {
 --      name: "user_filter",
 --      display: "User",
 --      type: "select",
 --      database_options: {
---          table: "time_entry_info",
+--          table: "user_drop",
 --          column: "user_name",
 --          all: true
 --      },
 --      multiple: false
 -- }
+-- VARIABLE: {
+--      name: "activity_filter",
+--      display: "Activity",
+--      type: "select",
+--      database_options: {
+--          table: "activity_drop",
+--          column: "activity_name",
+--          all: true
+--      },
+--      multiple: false
+-- }
+-- FILTER: {
+--      filter: "hide",
+--      column: "project_id"
+-- }
+-- FILTER: {
+--      filter: "hide",
+--      column: "lft"
+-- }
+-- FILTER: {
+--      filter: "hide",
+--      column: "depth"
+-- }
+-- FILTER: {
+--      filter: "hide",
+--      column: "time_entry_id"
+-- }
+-- FILTER: {
+--      filter: "hide",
+--      column: "user_id"
+-- }
+-- FILTER: {
+--      filter: "hide",
+--      column: "issue_id"
+-- }
+-- FILTER: {
+--      filter: "hide",
+--      column: "activity_id"
+-- }
 
 
-SELECT p.project_id, p.name, p.lft, p.depth,
-       t.time_entry_id, t.user_id, t.user_name,
-       t.issue_id, t.issue_subject, t.hours,
-  t.spent_on, t.activity_id, t.activity_name,
-       t.effort_values, t.comments
+SELECT p.project_id, p.name AS 'Project', p.lft, p.depth,
+       t.time_entry_id, t.user_id, t.user_name AS 'User',
+       t.issue_id, t.issue_subject AS 'Issue', t.hours AS 'Hours',
+       t.spent_on AS 'Date', t.activity_id, t.activity_name AS 'Activity',
+       t.effort_values AS 'Effort', t.comments AS 'Notes'
   FROM project_info p
   LEFT JOIN time_entry_info t
     ON t.project_id = p.project_id
  WHERE 1 = 1
    AND (t.spent_on BETWEEN "{{ date_range.start }}" AND "{{ date_range.end }}")
+   AND ("{{ project_filter }}" = "ALL" OR p.name = "{{ project_filter }}")
    AND ("{{ user_filter }}" = "ALL" OR t.user_name = "{{ user_filter }}")
+   AND ("{{ activity_filter }}" = "ALL" OR t.activity_name = "{{ activity_filter }}")
 /*
  WHERE 1 = 1
    AND (t.spent_on BETWEEN "{{ date_range.start }}" AND "{{ date_range.end }}")
