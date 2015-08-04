51f83c70c9b4a8490d3d6a4c07b26d5057c4b911
RutherfordMichaelW <mwrutherford@uams.edu>
2015-08-04 10:19:39 -0500

    added x-ua-compatibility to reports to improve IE

--- work/redmine_queries_v2.sql
+++ work/redmine_queries_v2.sql
@@ -92,93 +92,7 @@ SELECT @row := @row + 1 as row, comb.* FROM
  (SELECT @row := 0) r;
 
 
-# FINAL BASE QUERY - (using vars $USER and $DATE)
- 
-SELECT @row := @row + 1 as row, comb.* FROM 
-(SELECT proj.project_id, proj.issue_id, 
-       entries.id AS time_entry_id, entries.user_id,
-       CONCAT(REPEAT('     ', proj.depth), proj.name) AS name,
-       (SELECT CASE WHEN EXISTS (
-          SELECT * FROM bitnami_redmine.enabled_modules
-           WHERE project_id = proj.project_id AND name = 'time_tracking'
-        ) THEN TRUE ELSE FALSE END) AS allowTimeInput,
-       entries.hours, entries.spent_on, entries.comments,
-       entries.activity_id, entries.activity_name, 
-       entries.istri_cvid, entries.istri_value,
-       entries.effort_cvids, entries.effort_values,
-       entries.investigator_cvid, entries.investigator_value,
-       CASE WHEN entries.hours IS NULL THEN 'INSERT' ELSE 'UPDATE' END AS action_type,
-       0 AS action_edited
-  FROM (
-    SELECT issue.project_id , issue.id AS issue_id,
-           CONCAT("Issue ", CAST(issue.id AS CHAR), ": ", issue.subject) AS name,
-           (SELECT node.lft
-              FROM bitnami_redmine.projects AS node
-             WHERE node.id = issue.project_id) AS lft,       
-           (SELECT (COUNT(parent.name) - 1) AS depth
-              FROM bitnami_redmine.members
-              JOIN bitnami_redmine.projects AS node 
-                ON node.id = members.project_id
-              JOIN bitnami_redmine.projects AS parent 
-                ON node.lft BETWEEN parent.lft AND parent.rgt
-             WHERE node.status = 1 
-               AND node.id = issue.project_id 
-               AND members.user_id = $USER
-             GROUP BY node.name) + 1 AS depth
-      FROM bitnami_redmine.issues AS issue
-     WHERE issue.assigned_to_id = $USER
-        OR EXISTS (SELECT id FROM bitnami_redmine.watchers 
-                    WHERE watchers.watchable_type = 'Issue' 
-                      AND watchers.watchable_id = issue.id 
-                      AND watchers.user_id = $USER)
-     UNION ALL
-    SELECT node.id AS project_id, NULL AS issue_id,
-           node.name, node.lft, (COUNT(parent.name) - 1) AS depth
-      FROM bitnami_redmine.members
-      JOIN bitnami_redmine.projects AS node 
-        ON node.id = members.project_id
-      JOIN bitnami_redmine.projects AS parent 
-        ON node.lft BETWEEN parent.lft AND parent.rgt
-     WHERE node.status = 1 AND members.user_id = $USER
-     GROUP BY node.name) proj
-  LEFT JOIN (SELECT time_entries.id, time_entries.project_id,
-       time_entries.user_id, time_entries.issue_id,
-       time_entries.hours, time_entries.spent_on, time_entries.activity_id, 
-       IF(time_entries.comments = '', NULL, time_entries.comments) as comments,
-       (SELECT name FROM bitnami_redmine.enumerations
-         WHERE enumerations.type = 'TimeEntryActivity'
-           AND enumerations.id = time_entries.activity_id) AS activity_name,
-       (SELECT id
-          FROM bitnami_redmine.custom_values
-         WHERE custom_values.custom_field_id = 3
-           AND custom_values.customized_id = time_entries.id) AS istri_cvid,           
-       (SELECT value
-          FROM bitnami_redmine.custom_values
-         WHERE custom_values.custom_field_id = 3
-           AND custom_values.customized_id = time_entries.id) AS istri_value,
-       (SELECT GROUP_CONCAT(id ORDER BY id ASC SEPARATOR ', ')
-          FROM bitnami_redmine.custom_values
-         WHERE custom_values.custom_field_id = 4
-           AND custom_values.customized_id = time_entries.id) AS effort_cvids,
-       (SELECT GROUP_CONCAT(value ORDER BY id ASC SEPARATOR ', ')
-          FROM bitnami_redmine.custom_values
-         WHERE custom_values.custom_field_id = 4
-           AND custom_values.customized_id = time_entries.id) AS effort_values,
-       (SELECT id
-          FROM bitnami_redmine.custom_values
-         WHERE custom_values.custom_field_id = 10
-           AND custom_values.customized_id = time_entries.id) AS investigator_cvid,
-       (SELECT IF(value = '', NULL, value) as value
-          FROM bitnami_redmine.custom_values
-         WHERE custom_values.custom_field_id = 10
-           AND custom_values.customized_id = time_entries.id) AS investigator_value           
-  FROM bitnami_redmine.time_entries
- WHERE time_entries.user_id = $USER 
-   AND time_entries.spent_on = $DATE) entries 
-    ON entries.project_id = proj.project_id
-   AND IFNULL(entries.issue_id,0) = IFNULL(proj.issue_id,0)
- ORDER BY proj.lft, proj.depth) comb,
- (SELECT @row := 0) r
+
 
 #-----------------------------------
 # INSERT 
@@ -390,7 +304,7 @@ SELECT @row := @row + 1 as row, comb.* FROM
        entries.effort_cvids, entries.effort_values,
        entries.investigator_cvid, entries.investigator_value,
        CASE WHEN entries.hours IS NULL THEN 'INSERT' ELSE 'UPDATE' END AS action_type,
-       0 AS action_edited
+       0 AS action_edited, proj.closed_on
   FROM (
     SELECT issue.project_id , issue.id AS issue_id,
            CONCAT("Issue ", CAST(issue.id AS CHAR), ": ", issue.subject) AS name,
@@ -405,23 +319,25 @@ SELECT @row := @row + 1 as row, comb.* FROM
                 ON node.lft BETWEEN parent.lft AND parent.rgt
              WHERE node.status = 1 
                AND node.id = issue.project_id 
-               AND members.user_id = 30
-             GROUP BY node.name) + 1 AS depth
+               AND members.user_id = 41
+             GROUP BY node.name) + 1 AS depth,
+           issue.closed_on
       FROM bitnami_redmine.issues AS issue
-     WHERE issue.assigned_to_id = 30
+     WHERE issue.assigned_to_id = 41
         OR EXISTS (SELECT id FROM bitnami_redmine.watchers 
                     WHERE watchers.watchable_type = 'Issue' 
                       AND watchers.watchable_id = issue.id 
-                      AND watchers.user_id = 30)
-     UNION ALL
+                      AND watchers.user_id = 41)
+    UNION ALL
     SELECT node.id AS project_id, NULL AS issue_id,
-           node.name, node.lft, (COUNT(parent.name) - 1) AS depth
+           node.name, node.lft, (COUNT(parent.name) - 1) AS depth,
+           NULL AS closed_on
       FROM bitnami_redmine.members
       JOIN bitnami_redmine.projects AS node 
         ON node.id = members.project_id
       JOIN bitnami_redmine.projects AS parent 
         ON node.lft BETWEEN parent.lft AND parent.rgt
-     WHERE node.status = 1 AND members.user_id = 30
+     WHERE node.status = 1 AND members.user_id = 41
      GROUP BY node.name) proj
   LEFT JOIN (SELECT time_entries.id, time_entries.project_id,
        time_entries.user_id, time_entries.issue_id,
@@ -455,12 +371,14 @@ SELECT @row := @row + 1 as row, comb.* FROM
          WHERE custom_values.custom_field_id = 10
            AND custom_values.customized_id = time_entries.id) AS investigator_value           
   FROM bitnami_redmine.time_entries
- WHERE time_entries.user_id = 30 
-   AND time_entries.spent_on = '2015-05-31') entries 
+ WHERE time_entries.user_id = 41 
+   AND time_entries.spent_on = '2015-07-31') entries 
     ON entries.project_id = proj.project_id
    AND IFNULL(entries.issue_id,0) = IFNULL(proj.issue_id,0)
+ WHERE proj.closed_on IS NULL
+    OR '2015-07-31' < DATE_ADD(proj.closed_on, INTERVAL 7 DAY)
  ORDER BY proj.lft, proj.depth) comb,
- (SELECT @row := 0) r
+ (SELECT @row := 0) r 
 
 
 
